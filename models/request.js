20a17127a6e3f04b3510d05cc79b9d33314b92ac
andrewwinke123 <andrewwinke@gmail.com>
2024-03-03 16:35:14 -0500

    adding the request model for user requests

---
+++ models/request.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const requestSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: ['Commercial', 'Residential', 'Other'],
+    },
+   //add photos later^^^
+   //add photos later^^^
+   //add photos later^^^
+    author: { type: Schema.Types.ObjectId, ref: 'Profile' },
+    comments: [commentSchema]
+  },
+  { timestamps: true }
+)
+
+const Request = mongoose.model('request', RequestSchema)
+
+export { Request }
